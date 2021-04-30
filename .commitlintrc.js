52e7677a94095e512acf9d04f56dbab414838a60
Vahid Mohammadi <vahidm1467@gmail.com>
2021-05-01 04:12:27 +0430

    chore: add `test` to commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
 
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'style', 'improve', 'chore', 'docs']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'style', 'improve', 'chore', 'docs', 'test']],
     'scope-enum': [2, 'always', ['angular', 'rules']],
   },
 };
