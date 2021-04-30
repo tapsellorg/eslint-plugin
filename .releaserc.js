d990986359b1939aba9a873714eec38773c7b1a1
Vahid Mohammadi <vahidm1467@gmail.com>
2021-05-01 03:43:31 +0430

    chore: add github actions for semantic release

---
+++ .releaserc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  pkgRoot: 'dist',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'improve', release: 'patch' },
+          { breaking: true, release: 'minor' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'refactor', section: 'Refactors' },
+            { type: 'improve', section: 'Improvements' },
+          ],
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    // ['@semantic-release/exec', {}],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          "Release <%= nextRelease.version %> - <%= new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %> [skip ci]\n\n<%= nextRelease.notes %>",
+        assets: ['package.json', 'CHANGELOG.md'],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+  branches: ['master'],
+};
