e5f71e977eaeb6ebfecc917cf250db4f49c28b79
Vahid Mohammadi <vahidm1467@gmail.com>
2021-08-21 19:37:47 +0430

    perf: update packages and angular config

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,7 +2,11 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
 
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'style', 'improve', 'chore', 'docs', 'test']],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'style', 'improve', 'chore', 'docs', 'test', 'perf'],
+    ],
     'scope-enum': [2, 'always', ['angular', 'rules']],
   },
 };
