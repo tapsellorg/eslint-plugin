2aeaa2a55c4ef87f3691d0ffb6743f85ac5ff112
yasharsanayee <yasharsanayee@gmail.com>
2024-01-27 12:06:24 +0330

    improve: change base project rules to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
         createDefaultProgram: false,
       },
       plugins: [/*'rxjs',*/ 'sonarjs', 'unused-imports'],
-      extends: ['plugin:@typescript-eslint/stylistic', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/stylistic-type-checked', 'plugin:@typescript-eslint/strict-type-checked', 'plugin:sonarjs/recommended', 'plugin:@angular-eslint/recommended', 'prettier'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:sonarjs/recommended', 'plugin:@angular-eslint/recommended', 'prettier'],
       rules: {
         // Eslint
         'no-negated-in-lhs': 'error',
@@ -62,12 +62,12 @@ module.exports = {
           'warn',
           { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
         ],
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off', // This is producing a lot of warnings that aren't really important.
-        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn', // This is producing a lot of warnings that aren't really important.
+        '@typescript-eslint/no-unsafe-return': 'warn',
         '@typescript-eslint/no-unnecessary-condition': 'off', // TODO: uncomment when all projects are strict:true
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/prefer-nullish-coalescing': 'warn',
       },
     },
   ],
